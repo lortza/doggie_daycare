eeb0169eb1d534b0f7e319fbac1badabb6ba56f0
anne <richardson.ae@gmail.com>
2015-05-21 10:34:36 -0500

    broken ass state and rolled back state model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150521141256) do
+ActiveRecord::Schema.define(version: 20150521135825) do
 
   create_table "breeds", force: :cascade do |t|
     t.string   "name"
@@ -47,13 +47,6 @@ ActiveRecord::Schema.define(version: 20150521141256) do
     t.string   "emerg_phone"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
-  end
-
-  create_table "states", force: :cascade do |t|
-    t.string   "name"
-    t.string   "abbreviation"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
   end
 
   create_table "vets", force: :cascade do |t|
