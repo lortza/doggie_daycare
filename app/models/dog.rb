61b5c3537a213e4a64bacb9d2be5b7b0248d06e1
anne <richardson.ae@gmail.com>
2015-05-21 19:34:37 -0500

    updated buttons forms and styles along with alpha order

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,14 @@
+class Dog < ActiveRecord::Base
+  belongs_to :breed
+  belongs_to :owner
+  belongs_to :vet
+
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates :name, :breed_id, :birth_date, :owner_id, :vet_id, presence: true
+
+end
+
 # == Schema Information
 #
 # Table name: dogs
@@ -12,13 +23,3 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-
-class Dog < ActiveRecord::Base
-  belongs_to :breed
-  belongs_to :owner
-  belongs_to :vet
-
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-
-end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,22 +1,3 @@
-# == Schema Information
-#
-# Table name: owners
-#
-#  id             :integer          not null, primary key
-#  first_name     :string
-#  last_name      :string
-#  phone          :string
-#  street_address :string
-#  city           :string
-#  state          :string
-#  zip            :string
-#  signed_waiver  :boolean
-#  emerg_contact  :string
-#  emerg_phone    :string
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
-#
-
 class Owner < ActiveRecord::Base
   has_many :dogs
   belongs_to :state
@@ -31,9 +12,27 @@ class Owner < ActiveRecord::Base
 
   def address_block
     "#{street_address}, #{city}, ST #{zip}"
-
   end
 
-
+  validates :first_name, :last_name, :street_address, :city, :zip, presence: true, length: {minimum: 1}
+  validates :phone, presence: true
 
 end
+
+# == Schema Information
+#
+# Table name: owners
+#
+#  id             :integer          not null, primary key
+#  first_name     :string
+#  last_name      :string
+#  phone          :string
+#  street_address :string
+#  city           :string
+#  zip            :string
+#  signed_waiver  :boolean
+#  emerg_contact  :string
+#  emerg_phone    :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
