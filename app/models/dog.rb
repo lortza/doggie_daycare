cda5c25f0d30633a7728f0ba0e3edf500cd9a659
anne <richardson.ae@gmail.com>
2015-05-26 08:37:39 -0500

    updated models to make avatars square

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,9 +3,17 @@ class Dog < ActiveRecord::Base
   belongs_to :owner
   belongs_to :vet
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates :name, :breed_id, :birth_date, :owner_id, :vet_id, presence: true
+
+  has_attached_file :avatar, :styles => { 
+    :medium => "300x300#", 
+    :thumb => "100x100#" }, 
+    :default_url => "/images/:style/missing.png"
+
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  validates_attachment_size :avatar, :less_than => 3.megabytes,
+    :unless => Proc.new {|m| m[:avatar_file_name].blank?}
 
 end
 
