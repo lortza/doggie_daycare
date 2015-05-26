0109bb45bc147195c0228e071bfbc57950bfe1bd
anne <richardson.ae@gmail.com>
2015-05-26 09:18:29 -0500

    updated dog photos, changed avatar settings to square, added image for missing image dog

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -8,7 +8,7 @@ class Dog < ActiveRecord::Base
   has_attached_file :avatar, :styles => { 
     :medium => "300x300#", 
     :thumb => "100x100#" }, 
-    :default_url => "/images/:style/missing.png"
+    :default_url => "missing_:style.jpg" #points to app/assets/images/missing_medium.jpg
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
