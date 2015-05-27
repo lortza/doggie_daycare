4017b5fc7d0446603bbb4e312258ee13e5281fb3
anne <richardson.ae@gmail.com>
2015-05-27 07:34:56 -0500

    changed jumbotron styles to remove dog image from body background

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -14,6 +14,8 @@ class Owner < ActiveRecord::Base
     "#{street_address}, #{city}, ST #{zip}"
   end
 
+  
+
   validates :first_name, :last_name, :street_address, :city, :zip, presence: true, length: {minimum: 1}
   validates :phone, presence: true
 
