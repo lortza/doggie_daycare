c9b39251d3f3fca1a57cf960c6546ac4625698ce
anne <richardson.ae@gmail.com>
2015-06-02 08:46:36 -0500

    added menu dropdown and controller params for displaying categories in dropdown

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -1,5 +1,11 @@
 class StorefrontController < ApplicationController
   def all_items
-    @products = Product.all
-  end
-end
+    if params[:cat_id]
+      # TODO: add logic to handle when cat_id returns a category without products
+      @category = Category.find(params[:cat_id])
+      @products = Product.where(category_id: params[:cat_id])
+    else
+      @products = Product.all
+    end #if
+  end #def
+end #class
