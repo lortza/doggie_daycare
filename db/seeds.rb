c04dbb35fd18beebc2bfa3855852df832a86f9af
anne <richardson.ae@gmail.com>
2015-06-10 11:52:30 -0500

    css and scss and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,20 +24,17 @@ Breed.create(name: 'Mutt')
 
 categories = Category.create([{name: 'toys'}, {name: 'treats'}, {name: 'food'}, {name: 'accessories'}])
 
-Breed.create(name: 'Border Collie')
-Breed.create(name: 'Jack Russell')
-Breed.create(name: 'Pit Bull')
-Breed.create(name: 'Shih-tzu')
-Breed.create(name: 'Standard Poodle')
-Breed.create(name: 'Miniature Poodle')
-Breed.create(name: 'Doberman')
-Breed.create(name: 'Bulldog')
-Breed.create(name: 'Alaskan Malamute')
-Breed.create(name: 'Eskimo')
-Breed.create(name: 'Chihuahua')
-Breed.create(name: 'Yorkie')
-Breed.create(name: 'Boxer')
-Breed.create(name: 'Mutt')
+Vet.create(name: "Dr. Sbaitso", phone: "555-867-5309")
+Vet.create(name: "Dr. Zhivago", phone: "555-778-8265")
+Vet.create(name: "Dr. Who", phone: "555-678-2093")
+
+Owner.create(first_name: "Kelly", last_name: "Kapowski", phone: "504-555-5014", emerg_contact: "AC Slater", emerg_phone: "985-555-0235", street_address: "123 Main Street", zip: "70115")
+
+Owner.create(first_name: "Zack", last_name: "Morris", phone: "504-555-5014", emerg_contact: "Jacki", emerg_phone: "985-555-0235", street_address: "321 Broad Street", zip: "70115")
+
+Dog.create(name: "Falcor", breed_id: 1, owner_id: 1, vet_id: 1, in_daycare: true)
+Dog.create(name: "Ralphie", breed_id: 5, owner_id: 2, vet_id: 3, in_daycare: true)
+
 
 
 
